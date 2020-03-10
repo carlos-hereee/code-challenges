8079a1e91ff4a8da1207082ba1863cd38ef7eb49
ch04937 <97hernandez.c@gmail.com>
2020-03-10 10:05:37 -0700

    valleys

---
+++ README.md
@@ -0,0 +1,8 @@
+# Practice and solve coding chanllenges using the UPER Framework
+
+    - U understand
+    - P plan
+    - E execute
+    - R reflect
+
+Problems from Hacker Rank
---
+++ day1/main.py
@@ -0,0 +1,55 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+
+
+def countingValleys(n, s):
+    # s is a string of Us and Ds
+    # keep track of the height
+    height = 0
+    # keep track of the number of valley
+    valleys = 0
+    # have a variable that kepp track of the previous step
+
+    # keep a dict of elevation changes associated with { U: 1, D: -1 }
+
+    # loop through the string s
+    for step in s:
+        # depending on wheter we see a U or a D
+        if step == 'U':
+            # update the height counter accordingly
+            height += 1
+        # if the height counters return to 0
+            if height == 0:
+                valleys += 1
+        else:
+            height -= 1
+            # how do we know we reached a height of 0 as a result of
+            # climbing down from a hill vs climbing from a valley?
+            # keep track of the most recent step we took, so that we know if
+            # we climbed up from a valley
+            # increment our valleys counter
+    # return the valleys counter
+    return valleys
+
+    # it only counts as a valley if we start at sea level and make our way
+    # back to sea level
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
