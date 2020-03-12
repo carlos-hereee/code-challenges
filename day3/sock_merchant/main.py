a1b2429d21b054d6daa28168b024d69c9d99b767
ch04937 <97hernandez.c@gmail.com>
2020-03-12 13:22:50 -0700

    o(n) solution

--- day3/sock_merchant/main.py
+++ day3/sock_merchant/main.py
@@ -11,28 +11,20 @@ def sock_merchant(n, ar):
     '''
     # keep track of number of pairs
     pairs = 0
-    # keep track of the index that is being compared
-    current = 0
-    # while length and the current index do not match
-    while len(ar) > current:
-        # traverse the ar
-        for index, sock in enumerate(ar):
-            # index error check
-            if index > len(ar) - 1:
-                return pairs
-            # if a duplicate number is
-            if ar[current] == ar[index+1]:
-                # remove both numbers from the array
-                ar.pop(current)
-                ar.pop(index)
-                # add 1 to the pairs counter
-                pairs += 1
-            # else there is wanst not a a duplicate
-            else:
-                index += 1
-        # after ar traversal is done move to next current
-        current += 1
-    # return the number of pairs
+    # create a set
+    couple = set()
+    # traverse ar
+    for sock in ar:
+        # if a sock in ar is not in the set
+        if sock not in couple:
+            # add it to the set
+            couple.add(sock)
+        # else sock is already in the set
+        else:
+            # remove that sock in the set and add a pair
+            couple.remove(sock)
+            pairs += 1
+
     return pairs
 
 
@@ -40,4 +32,4 @@ def sock_merchant(n, ar):
 n = 7
 ar = [1, 2, 1, 2, 1, 3, 2]
 
-print(sock_merchant(n, ar))
+print('number of pairs should be 2: ', sock_merchant(n, ar))
