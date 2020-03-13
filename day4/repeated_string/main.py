a17e0d4c5c234dd88cb2b1009f66f1f6196d0991
ch04937 <97hernandez.c@gmail.com>
2020-03-13 14:32:31 -0700

    problem set up

---
+++ day4/repeated_string/main.py
@@ -0,0 +1,13 @@
+def repeated_string(s, n):
+    '''
+    print the 
+    s = is a string to repeat 
+    n = the number of a characters to consider
+
+    '''
+
+
+s = 'a'
+n = 10
+
+print(repeated_string(s, n))
